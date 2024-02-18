3d00dc31179fa3c94dc8bb8e71b8b6724b5aa708
Miguel Pineda <miguel.pineda@imedicalservices.com>
2024-02-18 09:21:06 -0500

    add jpa

--- src/main/java/AgapeApp/Models/Person.java
+++ src/main/java/AgapeApp/Models/Person.java
@@ -2,21 +2,21 @@ package AgapeApp.Models;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 
 @Data // Genera getters, setters, toString, equals y hashCode
 @NoArgsConstructor // Genera un constructor sin argumentos
 @AllArgsConstructor // Genera un constructor con todos los argumentos
-@Table(name = "person")
+@Entity
 public class Person {
 
     @Id
-    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
